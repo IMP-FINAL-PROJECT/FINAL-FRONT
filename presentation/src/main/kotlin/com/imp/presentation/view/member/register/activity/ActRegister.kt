134d52230f983bf706596595ce5f3c70d0b47ce3
김후정 <fujeong00@gmail.com>
2024-03-20 19:25:22 +0900

    FP-48 design: 회원가입 화면 구현 중

--- presentation/src/main/kotlin/com/imp/presentation/view/member/register/activity/ActRegister.kt
+++ presentation/src/main/kotlin/com/imp/presentation/view/member/register/activity/ActRegister.kt
@@ -1,24 +1,40 @@
 package com.imp.presentation.view.member.register.activity
 
+import android.content.Intent
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import com.imp.presentation.R
 import com.imp.presentation.base.BaseContractActivity
-import com.imp.presentation.databinding.ActMemberLoginBinding
+import com.imp.presentation.databinding.ActMemberRegisterBinding
+import com.imp.presentation.view.main.activity.ActMain
+import com.imp.presentation.widget.extension.hideKeyboard
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * Register Activity
  */
 @AndroidEntryPoint
-class ActRegister : BaseContractActivity<ActMemberLoginBinding>() {
+class ActRegister : BaseContractActivity<ActMemberRegisterBinding>() {
 
-    override fun getViewBinding() = ActMemberLoginBinding.inflate(layoutInflater)
+    override fun getViewBinding() = ActMemberRegisterBinding.inflate(layoutInflater)
 
     override fun initData() {
 
+        backPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+
+                hideKeyboard(this@ActRegister, currentFocus)
+                activityCloseAnimation()
+                finish()
+            }
+        }
     }
 
     override fun initView() {
 
         initDisplay()
+        initViewPager()
+        setOnClickListener()
     }
 
     /**
@@ -26,5 +42,52 @@ class ActRegister : BaseContractActivity<ActMemberLoginBinding>() {
      */
     private fun initDisplay() {
 
+        with(mBinding) {
+
+            incHeader.tvTitle.visibility = View.GONE
+
+            tvRegisterTitle.text = getString(R.string.register_text_1)
+
+            // 로그인 버튼
+            incNextAndRegister.apply {
+
+                tvButton.text = getString(R.string.next)
+                tvButton.isEnabled = false
+            }
+        }
+    }
+
+    /**
+     * Initialize ViewPager
+     */
+    private fun initViewPager() {
+
+        with(mBinding) {
+
+        }
+    }
+
+    /**
+     * Set onClickListener
+     */
+    private fun setOnClickListener() {
+
+        with(mBinding) {
+
+        }
+    }
+
+    /**
+     * Move to Main
+     */
+    private fun moveToMain() {
+
+        // 메인 화면 이동
+        Intent(this@ActRegister, ActMain::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(this)
+        }
+
+        finishAffinity()
     }
 }
